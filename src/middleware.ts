66959cbcc9dac550d65574d8550d9fe1cb7c17a9
bentahsin <tahsin@bentahsin.com>
2025-11-26 20:32:05 +0300

    Refactor authentication middleware to use NextAuth with updated configuration

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,8 @@
-import { auth } from './auth';
-export default auth;
+import NextAuth from 'next-auth';
+import { authConfig } from '../auth.config';
+
+export default NextAuth(authConfig).auth;
+
 export const config = {
-  matcher: ['/admin/:path*'],
+    matcher: ['/admin/:path*'],
 };

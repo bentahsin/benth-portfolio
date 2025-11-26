43919d3ed154cb95d2459c7f036d33438e45d3b8
root <root@QsqXP3N.nodesty.com>
2025-11-26 18:29:39 +0000

    fix: middleware

--- auth.config.ts
+++ auth.config.ts
@@ -1,28 +1,49 @@
 import type { NextAuthConfig } from 'next-auth';
 
 export const authConfig = {
-
   pages: {
     signIn: '/api/auth/signin',
   },
+  session: {
+    strategy: "jwt",
+  },
   callbacks: {
-
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
       const isOnAdminPanel = nextUrl.pathname.startsWith('/admin');
+      const userRole = auth?.user?.role;
+
+      if (isOnAdminPanel) {
+        console.log("🛡️  [MIDDLEWARE] Admin Kontrolü:");
+        console.log(`   -> Rol: '${userRole}'`);
+        console.log(`   -> Giriş: ${isLoggedIn}`);
+      }
 
       if (isOnAdminPanel) {
         if (isLoggedIn) {
-
-          if (auth.user?.role === 'ADMIN') {
-            return true;
-          }
-          return false;
+          return userRole === 'ADMIN';
         }
         return false;
       }
-
       return true;
+    },
+
+    async jwt({ token, user }) {
+      if (user) {
+        console.log("✅ [JWT] Rol Token'a yazılıyor:", user.role);
+        token.id = user.id;
+        token.role = user.role;
+      }
+      return token;
+    },
+    
+    async session({ session, token }) {
+      if (session.user && token) {
+        session.user.id = token.id as string;
+        // @ts-ignore
+        session.user.role = token.role as string;
+      }
+      return session;
     },
   },
   providers: [],
--- src/auth.ts
+++ src/auth.ts
@@ -2,9 +2,10 @@ import NextAuth from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import prisma from "./lib/prisma";
 import GitHub from "next-auth/providers/github";
-import { UserRole } from "@prisma/client";
+import { authConfig } from "../auth.config";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
+  ...authConfig,
   adapter: PrismaAdapter(prisma),
   providers: [
     GitHub({
@@ -12,38 +13,4 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
       clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
     }),
   ],
-  session: {
-    strategy: "jwt",
-  },
-  callbacks: {
-    async jwt({ token, user }) {
-      if (user) {
-        token.id = user.id;
-        token.role = user.role;
-      }
-      return token;
-    },
-
-    async session({ session, token }) {
-      if (session.user) {
-        session.user.id = token.id as string;
-        session.user.role = token.role as UserRole;
-      }
-      return session;
-    },
-
-    authorized({ auth, request: { nextUrl } }) {
-      const isLoggedIn = !!auth?.user;
-      const isOnAdminPanel = nextUrl.pathname.startsWith('/admin');
-
-      if (isOnAdminPanel) {
-        if (isLoggedIn) {
-          return auth.user?.role === UserRole.ADMIN;
-        }
-        return false;
-      }
-
-      return true;
-    },
-  },
 });
